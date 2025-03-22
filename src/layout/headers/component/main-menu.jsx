e0e371dd3b78a4c659f68076477b4e6bdcac3892
Naveenraj <nnaveenrajj@gmail.com>
2025-03-22 21:43:39 +0530

    Refactor main menu and submenu structure for improved accessibility and styling

--- src/layout/headers/component/main-menu.jsx
+++ src/layout/headers/component/main-menu.jsx
@@ -1,28 +1,69 @@
+import Link from "next/link";
 import React from "react";
 import menu_data from "../menu-data";
-import Link from "next/link";
 
 const MainMenu = () => {
   return (
     <ul className="mainmenu">
       {menu_data.map((menu, i) => (
         <li key={i} className="has-droupdown">
-          <a href={menu.link} className={!menu.submenus ? "no-dropdown" : ""}>
+          <Link
+            href={menu.link}
+            className={!menu.submenus ? "no-dropdown" : ""}
+          >
             {menu.title}
-          </a>
+          </Link>
+
+          {/* First-level dropdown */}
           {!menu.mega_menu && menu.submenus && (
             <ul className="submenu">
               {menu.submenus.map((nav, i) => (
-                <li key={i}>
-                  <Link href={`${nav.link}`}>
+                <li key={i} className="has-submenu">
+                  <Link href={nav.link}>
                     {nav.title}
                     {nav?.hot && <span className="badge-1">hot</span>}
                     {nav?.new && <span className="badge">new</span>}
                   </Link>
+                  {/* Second-level submenu */}
+                  {nav.submenus && (
+                    <ul className="mega-sub-menu-01">
+                      {nav.submenus.map((subNav, i) => (
+                        <li key={i} className="has-submenu">
+                          <Link href={subNav.link}>
+                            {subNav.title}
+                            {subNav?.hot && (
+                              <span className="badge-1">hot</span>
+                            )}
+                            {subNav?.new && <span className="badge">new</span>}
+                          </Link>
+                          {/* Third-level submenu */}
+                          {subNav.submenus && (
+                            <ul>
+                              {subNav.submenus.map((nav, i) => (
+                                <li key={i}>
+                                  <Link href={nav.link}>
+                                    {nav.title}
+                                    {nav?.hot && (
+                                      <span className="badge-1">hot</span>
+                                    )}
+                                    {nav?.new && (
+                                      <span className="badge">new</span>
+                                    )}
+                                  </Link>
+                                </li>
+                              ))}
+                            </ul>
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </li>
               ))}
             </ul>
           )}
+
+          {/* Mega Menu */}
           {menu.mega_menu && (
             <ul className="mega-menu">
               {menu.submenus.map((nav, i) => (
@@ -30,40 +71,48 @@ const MainMenu = () => {
                   <h6 className="menu-title">{nav.title}</h6>
                   <ul className="submenu mega-sub-menu-01">
                     {nav.mega_submenu.map((m, i) => (
-                      <li key={i}>
-                        <Link href={`${m.link}`}>
+                      <li key={i} className="has-submenu">
+                        <Link href={m.link}>
                           {m.title}
                           {m?.hot && <span className="badge-1">hot</span>}
                           {m?.new && <span className="badge">new</span>}
                         </Link>
-                        <ul>
-                          {m.submenus?.map((nav, i) => (
-                            <li key={i}>
-                              <Link href={`${nav.link}`}>
-                                {nav.title}
-                                {nav?.hot && (
-                                  <span className="badge-1">hot</span>
+                        {/* Second-level in Mega Menu */}
+                        {m.submenus && (
+                          <ul>
+                            {m.submenus.map((nav, i) => (
+                              <li key={i} className="has-submenu">
+                                <Link href={nav.link}>
+                                  {nav.title}
+                                  {nav?.hot && (
+                                    <span className="badge-1">hot</span>
+                                  )}
+                                  {nav?.new && (
+                                    <span className="badge">new</span>
+                                  )}
+                                </Link>
+                                {/* Third-level in Mega Menu */}
+                                {nav.submenus && (
+                                  <ul>
+                                    {nav.submenus.map((nav, i) => (
+                                      <li key={i}>
+                                        <Link href={nav.link}>
+                                          {nav.title}
+                                          {nav?.hot && (
+                                            <span className="badge-1">hot</span>
+                                          )}
+                                          {nav?.new && (
+                                            <span className="badge">new</span>
+                                          )}
+                                        </Link>
+                                      </li>
+                                    ))}
+                                  </ul>
                                 )}
-                                {nav?.new && <span className="badge">new</span>}
-                              </Link>
-                              <ul>
-                                {nav.submenus?.map((nav, i) => (
-                                  <li key={i}>
-                                    <Link href={`${nav.link}`}>
-                                      {nav.title}
-                                      {nav?.hot && (
-                                        <span className="badge-1">hot</span>
-                                      )}
-                                      {nav?.new && (
-                                        <span className="badge">new</span>
-                                      )}
-                                    </Link>
-                                  </li>
-                                ))}
-                              </ul>
-                            </li>
-                          ))}
-                        </ul>
+                              </li>
+                            ))}
+                          </ul>
+                        )}
                       </li>
                     ))}
                   </ul>
--- src/layout/headers/menu-data.js
+++ src/layout/headers/menu-data.js
@@ -9,133 +9,211 @@ const menu_data = [
   {
     title: "Courses & Trainings",
     link: "/courses",
-    mega_menu: true,
+    // mega_menu: true,
+    // submenus: [
+    //   {
+    //     title: "Professional Training",
+    //     mega_submenu: [
+    //       {
+    //         title: "Human Resource Training",
+    //         link: `/courses/${toKebabCase("Human Resource Training")}`,
+    //       },
+    //       {
+    //         title: "Logistics Management Training",
+    //         link: `/courses/${toKebabCase("Logistics Management Training")}`,
+    //         new: true,
+    //       },
+    //       {
+    //         title: "Digital Marketing Training",
+    //         link: `/courses/${toKebabCase("Digital Marketing Training")}`,
+    //       },
+    //       {
+    //         title: "Technology Training",
+    //         link: `#`,
+    //         hot: true,
+    //         submenus: [
+    //           {
+    //             title: "Software Development",
+    //             link: `/courses/${toKebabCase("Software Development")}`,
+    //             submenus: [
+    //               {
+    //                 title: "Full Stack Development",
+    //                 link: `/courses/${toKebabCase("Full Stack Development")}`,
+    //               },
+    //               {
+    //                 title: "Cloud Computing",
+    //                 link: `/courses/${toKebabCase("Cloud Computing")}`,
+    //               },
+    //               {
+    //                 title: "Database Management",
+    //                 link: `/courses/${toKebabCase("Database Management")}`,
+    //               },
+    //               {
+    //                 title: "Mobile App Development",
+    //                 link: `/courses/${toKebabCase("Mobile App Development")}`,
+    //               },
+    //               {
+    //                 title: "Cybersecurity & Ethical Hacking",
+    //                 link: `/courses/${toKebabCase(
+    //                   "Cybersecurity & Ethical Hacking"
+    //                 )}`,
+    //               },
+    //             ],
+    //           },
+    //           {
+    //             title: "Accounting",
+    //             link: `/courses/${toKebabCase("Accounting")}`,
+    //             submenus: [
+    //               {
+    //                 title: "Tally Level 1",
+    //                 link: `/courses/${toKebabCase("Tally Level 1")}`,
+    //               },
+    //               {
+    //                 title: "Tally Level 2",
+    //                 link: `/courses/${toKebabCase("Tally Level 2")}`,
+    //               },
+    //               {
+    //                 title: "Tally Level 3",
+    //                 link: `/courses/${toKebabCase("Tally Level 3")}`,
+    //               },
+    //             ],
+    //           },
+    //           {
+    //             title: "Data Analytics",
+    //             link: `/courses/${toKebabCase("Data Analytics")}`,
+    //             submenus: [
+    //               {
+    //                 title: "Tableau",
+    //                 link: `/courses/${toKebabCase("Tableau")}`,
+    //               },
+    //               {
+    //                 title: "Power BI",
+    //                 link: `/courses/${toKebabCase("Power BI")}`,
+    //               },
+    //             ],
+    //           },
+    //           {
+    //             title: "Microsoft Office Training",
+    //             link: `/courses/${toKebabCase("Microsoft Office Training")}`,
+    //             submenus: [
+    //               {
+    //                 title: "Microsoft Excel",
+    //                 link: `/courses/${toKebabCase("Microsoft Excel")}`,
+    //               },
+    //               {
+    //                 title: "Microsoft Word",
+    //                 link: `/courses/${toKebabCase("Microsoft Word")}`,
+    //               },
+    //               {
+    //                 title: "Microsoft PowerPoint",
+    //                 link: `/courses/${toKebabCase("Microsoft PowerPoint")}`,
+    //               },
+    //             ],
+    //           },
+    //           {
+    //             title: "Microsoft Office Specialist",
+    //             link: `/courses/${toKebabCase("Microsoft Office Specialist")}`,
+    //             submenus: [
+    //               {
+    //                 title: "MOS Excel",
+    //                 link: `/courses/microsoft-office-specialist-excel`,
+    //               },
+    //               {
+    //                 title: "MOS Word",
+    //                 link: `/courses/microsoft-office-specialist-word`,
+    //               },
+    //             ],
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     title: "Overseas Education and Training",
+    //     mega_submenu: [
+    //       {
+    //         title: "Singapore Programs",
+    //         description: "6-month Study and 6-month Internship Programs",
+    //         link: `/courses/${toKebabCase("Singapore Programs")}`,
+    //         hot: true,
+    //       },
+    //     ],
+    //   },
+    // ],
     submenus: [
       {
-        title: "Professional Training",
-        mega_submenu: [
+        title: "Accounting",
+        link: `/courses/${toKebabCase("Accounting")}`,
+        submenus: [
           {
-            title: "Human Resource Training",
-            link: `/courses/${toKebabCase("Human Resource Training")}`,
+            title: "Tally Level 1",
+            link: `/courses/${toKebabCase("Tally Level 1")}`,
           },
           {
-            title: "Logistics Management Training",
-            link: `/courses/${toKebabCase("Logistics Management Training")}`,
-            new: true,
+            title: "Tally Level 2",
+            link: `/courses/${toKebabCase("Tally Level 2")}`,
           },
           {
-            title: "Digital Marketing Training",
-            link: `/courses/${toKebabCase("Digital Marketing Training")}`,
-          },
-          {
-            title: "Technology Training",
-            link: `#`,
-            hot: true,
-            submenus: [
-              {
-                title: "Software Development",
-                link: `/courses/${toKebabCase("Software Development")}`,
-                submenus: [
-                  {
-                    title: "Full Stack Development",
-                    link: `/courses/${toKebabCase("Full Stack Development")}`,
-                  },
-                  {
-                    title: "Cloud Computing",
-                    link: `/courses/${toKebabCase("Cloud Computing")}`,
-                  },
-                  {
-                    title: "Database Management",
-                    link: `/courses/${toKebabCase("Database Management")}`,
-                  },
-                  {
-                    title: "Mobile App Development",
-                    link: `/courses/${toKebabCase("Mobile App Development")}`,
-                  },
-                  {
-                    title: "Cybersecurity & Ethical Hacking",
-                    link: `/courses/${toKebabCase(
-                      "Cybersecurity & Ethical Hacking"
-                    )}`,
-                  },
-                ],
-              },
-              {
-                title: "Accounting",
-                link: `/courses/${toKebabCase("Accounting")}`,
-                submenus: [
-                  {
-                    title: "Tally Level 1",
-                    link: `/courses/${toKebabCase("Tally Level 1")}`,
-                  },
-                  {
-                    title: "Tally Level 2",
-                    link: `/courses/${toKebabCase("Tally Level 2")}`,
-                  },
-                  {
-                    title: "Tally Level 3",
-                    link: `/courses/${toKebabCase("Tally Level 3")}`,
-                  },
-                ],
-              },
-              {
-                title: "Data Analytics",
-                link: `/courses/${toKebabCase("Data Analytics")}`,
-                submenus: [
-                  {
-                    title: "Tableau",
-                    link: `/courses/${toKebabCase("Tableau")}`,
-                  },
-                  {
-                    title: "Power BI",
-                    link: `/courses/${toKebabCase("Power BI")}`,
-                  },
-                ],
-              },
-              {
-                title: "Microsoft Office Training",
-                link: `/courses/${toKebabCase("Microsoft Office Training")}`,
-                submenus: [
-                  {
-                    title: "Microsoft Excel",
-                    link: `/courses/${toKebabCase("Microsoft Excel")}`,
-                  },
-                  {
-                    title: "Microsoft Word",
-                    link: `/courses/${toKebabCase("Microsoft Word")}`,
-                  },
-                  {
-                    title: "Microsoft PowerPoint",
-                    link: `/courses/${toKebabCase("Microsoft PowerPoint")}`,
-                  },
-                ],
-              },
-              {
-                title: "Microsoft Office Specialist",
-                link: `/courses/${toKebabCase("Microsoft Office Specialist")}`,
-                submenus: [
-                  {
-                    title: "MOS Excel",
-                    link: `/courses/microsoft-office-specialist-excel`,
-                  },
-                  {
-                    title: "MOS Word",
-                    link: `/courses/microsoft-office-specialist-word`,
-                  },
-                ],
-              },
-            ],
+            title: "Tally Level 3",
+            link: `/courses/${toKebabCase("Tally Level 3")}`,
           },
         ],
       },
       {
-        title: "Overseas Education and Training",
-        mega_submenu: [
+        title: "Data Analytics",
+        link: `/courses/${toKebabCase("Data Analytics")}`,
+        submenus: [
           {
-            title: "Singapore Programs",
-            description: "6-month Study and 6-month Internship Programs",
-            link: `/courses/${toKebabCase("Singapore Programs")}`,
-            hot: true,
+            title: "Python",
+            link: `/courses/${toKebabCase("Python")}`,
+          },
+          {
+            title: "R Programming",
+            link: `/courses/${toKebabCase("R Programming")}`,
+          },
+          {
+            title: "SQL Server",
+            link: `/courses/${toKebabCase("SQL Server")}`,
+          },
+          {
+            title: "Tableau",
+            link: `/courses/${toKebabCase("Tableau")}`,
+          },
+          {
+            title: "Power BI",
+            link: `/courses/${toKebabCase("Power BI")}`,
+          },
+        ],
+      },
+      {
+        title: "Microsoft Office Training",
+        link: `/courses/${toKebabCase("Microsoft Office Training")}`,
+        submenus: [
+          {
+            title: "Microsoft Excel",
+            link: `/courses/${toKebabCase("Microsoft Excel")}`,
+          },
+          {
+            title: "Microsoft Word",
+            link: `/courses/${toKebabCase("Microsoft Word")}`,
+          },
+          {
+            title: "Microsoft PowerPoint",
+            link: `/courses/${toKebabCase("Microsoft PowerPoint")}`,
+          },
+        ],
+      },
+      {
+        title: "Microsoft Office Specialist",
+        link: `/courses/${toKebabCase("Microsoft Office Specialist")}`,
+        submenus: [
+          {
+            title: "MOS Excel",
+            link: `/courses/microsoft-office-specialist-excel`,
+          },
+          {
+            title: "MOS Word",
+            link: `/courses/microsoft-office-specialist-word`,
           },
         ],
       },
